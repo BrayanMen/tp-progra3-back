a4ec78e9208a9be41eb03ec2b7f7fd4ff2dd9545
Brayan Mendoza <95663591+BrayanMen@users.noreply.github.com>
2025-06-23 22:20:50 +0000

    controller

--- backend/controllers/products.controller.js
+++ backend/controllers/products.controller.js
@@ -3,18 +3,20 @@ import {connection} from "../database/db.js"
 export const getProducts = async (req, res) =>{
     try {
         // res.json({message: "trayendo productos"})
-        let sql = `SELECT * FROM products`
+        let sql = `SELECT * FROM products WHERE available = "active"`
         const [rows] = await connection.query(sql)
-        res.status(200).json( rows)
-        
+        res.status(200).json(rows)
+    
     } catch (err) {
         console.error("Error: ", err);        
     }
 }
 
 export const getProductById = async(req, res) =>{
-    const id = req.params.id
-    res.json({message: `trayendo producto id ${id}`})
+    const {id} = req.params
+    const sentence = `SELECT * FROM products where id = ?`
+    const [rows] = await connection.query(sentence,[id])
+    res.json(rows)
 }
 
 export const createProduct = async (req, res) =>{
